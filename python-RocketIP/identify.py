f1736755b76f6c143a0d4220bada1c60e9d40bfa
Daniel Thurau <Daniel.N.Thurau@gmail.com>
2017-12-10 10:45:03 -0800

    frtiz

--- python-RocketIP/identify.py
+++ python-RocketIP/identify.py
@@ -9,36 +9,53 @@ f = misc.imread('export.png', mode='I')
 im = np.zeros((256, 256))
 im[64:-64, 64:-64] = 1
 
+print(im)
+target = open("im.txt", 'w')
 
-# print(im)
-# print(f)
+for i in range(0, len(im)):
+	for j in range(0, len(im[i])):
+		target.write(str(im[i][j]) + ",")
+	target.write("\n")
 
-sx = ndimage.sobel(f, axis=0, mode='constant')
-sy = ndimage.sobel(f, axis=1, mode='constant')
-sob = np.hypot(sx, sy)
+# # print(im)
+# # print(f)
 
-
-print(sob)
-
-# target = open("sx.txt", 'w')
-
-
-# for i in range(0, len(sx)):
-# 	for j in range(0, len(sx[i])):
-# 		target.write(str(sx[i][j]) + ",")
-
-# target = open("sy.txt", 'w')
-
-# for i in range(0, len(sy)):
-# 	for j in range(0, len(sy[i])):
-# 		target.write(str(sy[i][j]) + ",")
+# sx = ndimage.sobel(f, axis=0, mode='constant')
+# sy = ndimage.sobel(f, axis=1, mode='constant')
+# sob = np.hypot(sx, sy)
 
 
-plt.imshow(sob)
-plt.axis('off')
-plt.show()
+# print(sob)
 
-plt.imshow(f)
-plt.axis('off')
-plt.show()
+# target = open("sob.txt", 'w')
+
+
+# for i in range(0, len(sob)):
+# 	for j in range(0, len(sob[i])):
+# 		target.write(str(sob[i][j]) + ",")
+# 	target.write("\n")
+
+# target = open("export.txt", 'w')
+
+# for i in range(0, len(f)):
+# 	for j in range(0, len(f[i])):
+# 		target.write(str(f[i][j]) + ",")
+# 	target.write("\n")
+
+
+# plt.imshow(sx)
+# plt.axis('off')
+# plt.show()
+
+# plt.imshow(sy)
+# plt.axis('off')
+# plt.show()
+
+# plt.imshow(sob)
+# plt.axis('off')
+# plt.show()
+
+# plt.imshow(f)
+# plt.axis('off')
+# plt.show()
 
