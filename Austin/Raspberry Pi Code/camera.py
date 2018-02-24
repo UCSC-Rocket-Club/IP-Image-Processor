dd73806e6bcbc8e1f55f967bb2dc332981ec89d4
AustianoSC <arlamb@ucsc.edu>
2018-02-24 12:03:20 -0800

    Updated Code

--- Austin/Raspberry Pi Code/camera.py
+++ Austin/Raspberry Pi Code/camera.py
@@ -1,44 +1,30 @@
 #import picamera
 import time
-#import shutil
-#import os
+import shutil
+import os
 import detect_color as dc
 import cv2
 
 def record():
-
+            
     #shutil.rmtree('/home/pi/images/')
     #os.mkdir('/home/pi/images')
-    #with picamera.PiCamera() as camera:
-    	#camera.resolution = (1280,720)
-        #camera.exposure_mode = 'sports'
-        #folder = '/home/pi/images/'
-        #while(True):
-            #time_stamp = str(time.time())
-            #fn = time_stamp
-            #camera.start_preview()
-            #camera.start_recording(folder + fn + '.h264')
-            #time.sleep(2)
-            #camera.stop_recording()
-            #camera.stop_preview()
-            #camera.capture(folder + fn + '.jpg', format='jpeg')
-            #dc.process(folder+fn + '.jpg')
-            
-    
+    #folder = '/home/pi/images/'
     cap = cv2.VideoCapture(0)
-    clock = 0
-    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+    fourcc = cv2.VideoWriter_fourcc('X','2','6','4')
+    
     while True:
+        clock = 0
         timeStamp = str(time.time())
         video = cv2.VideoWriter(timeStamp + '.avi', fourcc, 20, (640,480))
         
-        while clock < 2000:
+        while clock < 2:
             _, frame = cap.read()
             startTime = time.clock()
             frame = dc.process(frame)
+            cv2.imshow('Video',frame)
             clock += time.clock() - startTime
             video.write(frame)
-            print clock
             if cv2.waitKey(5) == 27:
                 break
         video.release()
@@ -46,3 +32,4 @@ def record():
                 break
 
     cap.release()
+    cv2.destroyAllWindows()
--- Austin/Raspberry Pi Code/detect_color.py
+++ Austin/Raspberry Pi Code/detect_color.py
@@ -26,65 +26,6 @@ def adjust_gamma(image, gamma=1.0):
 
 def process(frame):
 
-
-    # load the image and resize it to a smaller factor so that
-    # the shapes can be approximated better
-    #image = cv2.imread(x)
-    #resized = imutils.resize(image, width=300)
-    #ratio = image.shape[0] / float(resized.shape[0])
-
-
-    #adjusted = adjust_gamma(resized, gamma=1.0)
-
-
-    # blur the resized image slightly, then convert it to both
-    # grayscale and the L*a*b* color spaces
-    #blurred = cv2.GaussianBlur(adjusted, (5, 5), 0)
-    #gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-    #lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
-    #thresh = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY_INV)[1]
-
-# find contours in the thresholded image
-    #cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-    	#cv2.CHAIN_APPROX_SIMPLE)
-    #cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-    # initialize the shape detector and color labeler
-    # sd = ShapeDetector()
-    #cl = ColorLabeler()
-
-    # loop over the contours
-    #for c in cnts:
-	# compute the center of the contour
-	#M = cv2.moments(c)
-	#if M["m00"] == 0:
-		# M["m00"] = 1
-		#continue
-	#cX = int((M["m10"] / M["m00"]) * ratio)
-	#cY = int((M["m01"] / M["m00"]) * ratio)
-
-	# detect the shape of the contour and label the color
-	# shape = sd.detect(c)
-	#color = cl.label(lab, c)
-
-	# multiply the contour (x, y)-coordinates by the resize ratio,
-	# then draw the contours and the name of the shape and labeled
-	# color on the image
-	#c = c.astype("float")
-	#c *= ratio
-	#c = c.astype("int")
-	#text = "{}".format(color)
-	#if text != "NaN":
-	#	cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
-	#	cv2.putText(image, text, (cX, cY),
-	#		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-	#else:
-	#	cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
-
-	# show the output image
-    #cv2.imwrite(x,image)
-    # Define the codec and create VideoWriter object
-
     # Set Range of blue to be tracked
     lowerBlue = np.array([100,100,100])
     upperBlue = np.array([140,255,255])
