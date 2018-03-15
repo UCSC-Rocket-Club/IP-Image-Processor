e7a75bd6d3a0c392b389462ffaa46a8615b99699
AustianoSC <35877742+AustianoSC@users.noreply.github.com>
2018-03-14 20:15:44 -0700

    Fixed some indenting \

--- Austin/Raspberry Pi Code/ActualCode/pi3/camera.py
+++ Austin/Raspberry Pi Code/ActualCode/pi3/camera.py
@@ -1,69 +1,57 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-from adxl345 import ADXL345
-from multiprocessing import Process
-import detect_color
-import write_image
-import math
+import camera
+import detect_color as dc
 import time
-import shutil
-import os
-import cv2
+import math
+from multiprocessing import Process, Queue, Lock
+from adxl345 import ADXL345
+import RPi.GPIO as GPIO
 
-def record():
-            
-    shutil.rmtree('/home/pi/images/')
-    os.mkdir('/home/pi/images')
-    clock = 0
-    camera = PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 30
-    rawCapture = PiRGBArray(camera, size=(640, 480))
-    startTime = time.clock()
+def thread():
+    #p1 = Process(target=camera.record)
 
-    frames_record_time  = time.time()
-    frame_count = 0
+    print('\nBuckle up, butter cup. You just flipped my bitch switch\n')
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(10, GPIO.LOW)
+    print('Set-up Pin 10\n')
+    GPIO.setup(21, GPIO.LOW)
+    GPIO.output(21, GPIO.HIGH)
+    print('Set-up Pin 21\n')
+	
+    adxl = ADXL345()
+    print('Set-up ADXL345\n')
+
+    GPIO.output(10, GPIO.HIGH)
         
-    #start of multi-thread code
+    while True:
+        axes = adxl.getAxes(True)
+        xyforce = math.sqrt(axes['x'] ** 2 + axes['y'] ** 2)
+        netforce = math.sqrt(xyforce ** 2 + axes['z'] ** 2)
 
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        writing_process = Process(target=write_image.process, args=(frame, ))
-        writing_process.start()
-
-        frame_count += 1
-        clock = time.clock() - startTime
-        print('time: ' + str(clock))
-        rawCapture.truncate(0)
-
-        if clock > 10:
-            print('Camera Done ' + str(clock) +'s\n')
-            rawCapture.close()
-            camera.close()
+        if(netforce >= 5):
+            GPIO.output(10, GPIO.LOW)
+            print('LAUNCH LAUNCH LAUNCH LAUNCH LAUNCH')
             break
+    camera.record()
+    #p1.start()
 
-    frames_record_time = time.time() - frames_record_time
-    print(str(frame_count) + ' frames done in ' + str(frames_record_time) + ' seconds for the camera recording')     
+    # commented out to experiment with several threads writing images to the sd card at once
 
-    detect_color.anal_video()
+    #    p1.start()
+    #    adxl = ADXL345()
+    #
+    #    while True:
+    #        axes = adxl.getAxes(True)
+    #        xyforce = math.sqrt(axes['x'] ** 2 + axes['y'] ** 2)
+    #        netforce = math.sqrt(xyforce ** 2 + axes['z'] ** 2)
+    #
+    #        if(netforce >= 2):
+    #            print('LAUNCH LAUNCH LAUNCH LAUNCH LAUNCH')
+    #            break
+    #
+    #
+    #    time.sleep(2)
+    #    p2.start()
+
     
-    # old code that used to work, but wrote slowly
-
-#    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-#        image = frame.array
-#        timeStamp = str(time.time())
-#        cv2.imwrite(folder + timeStamp + ".jpg", image)
-#        frame_count += 1
-#        clock = time.clock() - startTime
-#        print('time: ' + str(clock))
-#        print('the ' + timeStamp + '.jpg frame has been taken and recorded \n')
-#        rawCapture.truncate(0)
-#        if clock > 10:
-#            print('Camera Done ' + str(clock) +'s\n')
-#            rawCapture.close()
-#            camera.close()
-#            break
-#
-#    frames_record_time = time.time() - frames_record_time
-#    print(str(frame_count) + ' frames done in ' + str(frames_record_time) + ' seconds for the camera recording')     
-#    
-
+thread()
